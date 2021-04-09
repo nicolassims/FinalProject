0094b4e6b0d5c1a8453ea74dec4985de9ba160dd
Ted Kiriakidi <fedorkiriakidi@gmail.com>
2021-04-09 16:47:54 -0400

    Added logging

--- server/lib/final_project_web/plugs/require_auth.ex
+++ server/lib/final_project_web/plugs/require_auth.ex
@@ -4,16 +4,15 @@ defmodule FinalProjectWeb.Plugs.RequireAuth do
   def init(args), do: args
 
   def call(conn, _args) do
-    Console.log("CHECKING AUTHORIZATION")
+    IO.inspect("AUTHORIZING")
     token = Enum.at(get_req_header(conn, "x-auth"), 0)
     case Phoenix.Token.verify(conn, "user_id",
           token, max_age: 86400) do
       {:ok, user_id} ->
-        Console.log("GRANTING")
         user = FinalProject.Users.get_user!(user_id)
         assign(conn, :current_user, user)
       {:error, err} ->
-        Console.log("UNAUTHED")
+        IO.inspect("ERROR AUTHORIZING")
         conn
         |> put_resp_header(
           "content-type", "application/json; charset=UTF-8")
